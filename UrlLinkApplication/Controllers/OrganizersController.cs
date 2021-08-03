3574ea56b6f465ae6d26c049ac36f59555d814e7
tracy <tracylee@wondercore.com>
2021-08-03 14:19:49 +0800

    Add project files.

---
+++ UrlLinkApplication/Controllers/OrganizersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UrlLinkApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrganizersController : Controller
+    {
+        [HttpGet("{id:int}")]
+        public IActionResult GetAction(int id)
+        {
+            return Ok($"get parameter：'{id}'");
+        }
+    }
+}
